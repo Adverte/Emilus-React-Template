140752381c5b162082593ca808c3c13590f9ecf2
Adverte <nomail>
2023-07-13 00:49:03 +0300

    fix useDrop and main landing

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ import MainLayout from "./pages/MainLayout";
 import UserTable from "./pages/UserTable";
 
 function App() {
+
     const content =
         <div className="App">
+            <h1>AWD</h1>
             <Provider store={store}>
                 <Router>
                     <Routes>
                         <Route path='/' element={<MainLayout/>}>
                             <Route index element={<UserTable/>}/>
-                            <Route path='*' element={<UserTable/>}/>
+                            <Route path='*' element={<HomePage/>}/>
                         </Route>
                     </Routes>
                 </Router>
             </Provider>
         </div>
+
+    return content;
 }
 
 export default App;
--- src/views/app-views/components/data-display/table/DragSorting.js
+++ src/views/app-views/components/data-display/table/DragSorting.js
@@ -1,158 +1,182 @@
-import React, { Component } from "react";
-import { Table } from "antd";
-import { DndProvider, useDrag, useDrop } from "react-dnd";
+import React, {Component} from "react";
+import {Table} from "antd";
+import {DndProvider, useDrag, useDrop} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import update from "immutability-helper";
 
 let dragingIndex = -1;
 
 class BodyRow extends React.Component {
-  render() {
-    const {
-      isOver,
-      connectDragSource,
-      connectDropTarget,
-      moveRow,
-      ...restProps
-    } = this.props;
-    const style = { ...restProps.style, cursor: "move" };
+    render() {
+        const {
+            isOver,
+            connectDragSource,
+            connectDropTarget,
+            moveRow,
+            ...restProps
+        } = this.props;
+        const style = {...restProps.style, cursor: "move"};
 
-    let { className } = restProps;
-    if (isOver) {
-      if (restProps.index > dragingIndex) {
-        className += " drop-over-downward";
-      }
-      if (restProps.index < dragingIndex) {
-        className += " drop-over-upward";
-      }
+        let {className} = restProps;
+        if (isOver) {
+            if (restProps.index > dragingIndex) {
+                className += " drop-over-downward";
+            }
+            if (restProps.index < dragingIndex) {
+                className += " drop-over-upward";
+            }
+        }
+
+        return connectDragSource(
+            connectDropTarget(
+                <tr {...restProps} className={className} style={style}/>
+            )
+        );
     }
-
-    return connectDragSource(
-      connectDropTarget(
-        <tr {...restProps} className={className} style={style} />
-      )
-    );
-  }
 }
 
 const rowSource = {
-  beginDrag(props) {
-    dragingIndex = props.index;
-    return {
-      index: props.index
-    };
-  }
+    beginDrag(props) {
+        dragingIndex = props.index;
+        return {
+            index: props.index
+        };
+    }
 };
 
 const rowTarget = {
-  drop(props, monitor) {
-    const dragIndex = monitor.getItem().index;
-    const hoverIndex = props.index;
+    drop(props, monitor) {
+        const dragIndex = monitor.getItem().index;
+        const hoverIndex = props.index;
 
-    // Don't replace items with themselves
-    if (dragIndex === hoverIndex) {
-      return;
+        // Don't replace items with themselves
+        if (dragIndex === hoverIndex) {
+            return;
+        }
+
+        // Time to actually perform the action
+        props.moveRow(dragIndex, hoverIndex);
+
+        // Note: we're mutating the monitor item here!
+        // Generally it's better to avoid mutations,
+        // but it's good here for the sake of performance
+        // to avoid expensive index searches.
+        monitor.getItem().index = hoverIndex;
     }
-
-    // Time to actually perform the action
-    props.moveRow(dragIndex, hoverIndex);
-
-    // Note: we're mutating the monitor item here!
-    // Generally it's better to avoid mutations,
-    // but it's good here for the sake of performance
-    // to avoid expensive index searches.
-    monitor.getItem().index = hoverIndex;
-  }
 };
 
-const DragableBodyRow = useDrop("row", rowTarget, (connect, monitor) => ({
-  connectDropTarget: connect.dropTarget(),
-  isOver: monitor.isOver()
-}))(
-    useDrag("row", rowSource, connect => ({
-    connectDragSource: connect.dragSource()
-  }))(BodyRow)
-);
+
+
+// const DragableBodyRow = DropTarget("row", rowTarget, (connect, monitor) => ({
+//     connectDropTarget: connect.dropTarget(),
+//     isOver: monitor.isOver()
+// }))(
+//     DragSource("row", rowSource, connect => ({
+//         connectDragSource: connect.dragSource()
+//     }))(BodyRow)
+// );
+
+const DragableBodyRow = ({ children }) => {
+    const [{ isOver }, connectDropTarget] = useDrop(() => ({
+        accept: 'row',
+        drop: (item) => {
+            // Handle drop logic
+        },
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+        }),
+    }));
+
+    const [, connectDragSource] = useDrag(() => ({
+        item: { type: 'row' },
+    }));
+    //
+    // return (
+    //     <tr ref={connectDropTarget}>
+    //         {React.cloneElement(children, { ref: connectDragSource })}
+    //     </tr>
+    // );
+};
 
 const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name"
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age"
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address"
-  }
+    {
+        title: "Name",
+        dataIndex: "name",
+        key: "name"
+    },
+    {
+        title: "Age",
+        dataIndex: "age",
+        key: "age"
+    },
+    {
+        title: "Address",
+        dataIndex: "address",
+        key: "address"
+    }
 ];
 
 export class DragSorting extends Component {
-  state = {
-    data: [
-      {
-        key: "1",
-        name: "John Brown",
-        age: 32,
-        address: "New York No. 1 Lake Park"
-      },
-      {
-        key: "2",
-        name: "Jim Green",
-        age: 42,
-        address: "London No. 1 Lake Park"
-      },
-      {
-        key: "3",
-        name: "Joe Black",
-        age: 32,
-        address: "Sidney No. 1 Lake Park"
-      }
-    ]
-  };
+    state = {
+        data: [
+            {
+                key: "1",
+                name: "John Brown",
+                age: 32,
+                address: "New York No. 1 Lake Park"
+            },
+            {
+                key: "2",
+                name: "Jim Green",
+                age: 42,
+                address: "London No. 1 Lake Park"
+            },
+            {
+                key: "3",
+                name: "Joe Black",
+                age: 32,
+                address: "Sidney No. 1 Lake Park"
+            }
+        ]
+    };
 
-  components = {
-    body: {
-      row: DragableBodyRow
-    }
-  };
-
-  moveRow = (dragIndex, hoverIndex) => {
-    const { data } = this.state;
-    const dragRow = data[dragIndex];
-
-    this.setState(
-      update(this.state, {
-        data: {
-          $splice: [
-            [dragIndex, 1],
-            [hoverIndex, 0, dragRow]
-          ]
+    components = {
+        body: {
+            row: DragableBodyRow
         }
-      })
-    );
-  };
+    };
 
-  render() {
-    return (
-      <DndProvider backend={HTML5Backend}>
-        <Table
-          columns={columns}
-          dataSource={this.state.data}
-          components={this.components}
-          onRow={(record, index) => ({
-            index,
-            moveRow: this.moveRow
-          })}
-        />
-      </DndProvider>
-    );
-  }
+    moveRow = (dragIndex, hoverIndex) => {
+        const {data} = this.state;
+        const dragRow = data[dragIndex];
+
+        this.setState(
+            update(this.state, {
+                data: {
+                    $splice: [
+                        [dragIndex, 1],
+                        [hoverIndex, 0, dragRow]
+                    ]
+                }
+            })
+        );
+    };
+
+    render() {
+        return (
+            <DndProvider backend={HTML5Backend}>
+                <Table
+                    columns={columns}
+                    dataSource={this.state.data}
+                    components={this.components}
+                    onRow={(record, index) => ({
+                        index,
+                        moveRow: this.moveRow
+                    })}
+                />
+            </DndProvider>
+        );
+    }
 }
 
 export default DragSorting;
